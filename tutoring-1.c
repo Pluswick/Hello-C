e9c36bdb7c7d4b04448e25d1ae22e42b06ce801d
DESKTOP-3GOTUVS\tjdgk <tjdgks0814@gmail.com>
2021-04-15 16:08:16 +0900

    tutoring-1.c
    1st code in tutoring

---
+++ tutoring-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main() {
+	char x1;
+	int x2;
+	float x3;
+	double x4;
+	
+	x1 = 'a';
+	x2 = 123;
+	x3 = 3.14;
+	x4 = 3.141592;
+
+	printf("x1=%c,x2=%d,x3=%f,x4=%f\n", x1, x2, x3, x4);
+	printf("Hello, World\n");
+
+	return 0;
+
+}
